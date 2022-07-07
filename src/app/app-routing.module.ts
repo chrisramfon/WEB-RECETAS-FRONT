aac97d1403a0961b141afbf7aa705cc5dcd2b2f8
NormaG123 <ng558925@gmail.com>
2022-07-06 19:01:42 -0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentess
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  //Rutas por defecto, dejar al final
+  {path:'',redirectTo:'inicio', pathMatch:'full'},
+  {path:'**', redirectTo:'inicio',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+//librerias
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
